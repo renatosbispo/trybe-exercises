26986f699c3b139b7da20feb3a9090ae86da981a
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-03-25 15:42:25 -0300

    Finish exercise 3

---
+++ bloco_4/dia_2/exercise_3.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let average = 0;
+
+for (let number of numbers) {
+  sum += number;
+}
+
+if (sum != 0) {
+  average = sum / numbers.length;
+}
+
+console.log(average);
