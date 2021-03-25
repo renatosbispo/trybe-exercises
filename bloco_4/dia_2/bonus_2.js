6cfdfd8a50b54a96fb0a3260e064cb694f0814b6
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-03-25 19:11:12 -0300

    Substitute variable 'offset' for already existing variable 'indexA'

--- bloco_4/dia_2/bonus_2.js
+++ bloco_4/dia_2/bonus_2.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbersSize = numbers.length;
-let indexA, indexB, offset = 1;
+let indexA, indexB;
 
-for (indexA = 0; indexA < numbersSize - 2; indexA += 1, offset += 1) {
-  for (indexB = 0; indexB < numbersSize - offset; indexB += 1) {
+for (indexA = 1; indexA < numbersSize - 1; indexA += 1) {
+  for (indexB = 0; indexB < numbersSize - indexA; indexB += 1) {
     num1 = numbers[indexB];
     num2 = numbers[indexB + 1];
     if (num1 < num2) {
