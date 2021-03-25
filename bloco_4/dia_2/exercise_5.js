3ad9c2b8761909bc411a7c7e32924ab9238cee1e
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-03-25 16:15:41 -0300

    Finish exercise 5

---
+++ bloco_4/dia_2/exercise_5.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let biggestNum = numbers[0];
+
+for (let number of numbers) {
+  if (number > biggestNum) {
+    biggestNum = number;
+  }
+}
+
+console.log(biggestNum);
