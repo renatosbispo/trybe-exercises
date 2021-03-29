8e70e9756ccc649bfb932c580c01b29e570ef18b
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-03-29 16:15:00 -0300

    Finish function exercise 2

--- bloco_4/dia_4/functions.js
+++ bloco_4/dia_4/functions.js
@@ -1,3 +1,4 @@
+// Exercise 1
 function isPalindrome(word) {
   let wordReversed = '';
   let answer = false;
@@ -14,4 +15,49 @@ function isPalindrome(word) {
   return answer;
 }
 
-console.log(isPalindrome('madam'));
+// Exercise 2
+function biggestNumberIndex(numbers) {
+  let biggestNumber;
+  let biggestNumberIndex;
+  
+  if (numbers.length == 0) {
+    biggestNumberIndex = null;
+  } else {
+    biggestNumber = numbers[0];
+    biggestNumberIndex = 0;
+    if (numbers.length > 1) {
+      for (let index = 1; index < numbers.length; index += 1) {
+        if (numbers[index] > biggestNumber) {
+          biggestNumberIndex = index;
+        }
+      }
+    }
+  }
+
+  return biggestNumberIndex;
+}
+
+// Exercise 2
+function biggestNumberIndex(numbers) {
+  let biggestNumber;
+  let biggestNumberIndex;
+  
+  if (numbers.length == 0) {
+    biggestNumberIndex = null;
+  } else {
+    biggestNumber = numbers[0];
+    biggestNumberIndex = 0;
+    if (numbers.length > 1) {
+      for (let index = 1; index < numbers.length; index += 1) {
+        if (numbers[index] > biggestNumber) {
+          biggestNumberIndex = index;
+        }
+      }
+    }
+  }
+
+  return biggestNumberIndex;
+}
+
+console.log('isPalindrome:', isPalindrome('madam'));
+console.log('biggestNumberIndex:', biggestNumberIndex([2, 3, 6, 7, 10, 1]));
