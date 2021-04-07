f12e7d75a73bd47a59f2758de96e49522602cc90
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-07 19:17:26 -0300

    Finish content exercise 4

--- bloco5/dia3/main.js
+++ bloco5/dia3/main.js
@@ -4,8 +4,22 @@ const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-/*Crie uma função que adicione a classe 'tech' ao elemento selecionado.
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso? */
+// Get random number between 0 and 1, including both.
+function inclusiveRandom() {
+  return Math.ceil(Math.random() * 1000000000000) / 1000000000000;
+}
+
+// Get random integer between min and max, including both.
+// Adapted from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+  return Math.floor(inclusiveRandom() * (max - min) + min);
+}
+
+/*
+ * Crie uma função que adicione a classe 'tech' ao elemento selecionado.
+ * 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+ */
 function toggleTechClass(event) {
   let element = event.target;
   let element2 = document.querySelector('.tech');
@@ -24,8 +38,10 @@ for (let box of boxes) {
   box.addEventListener('click', toggleTechClass);
 }
 
-/* Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech'. */
+/*
+ * Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+ * com a classe 'tech'.
+ */
 function changeTechText(event) {
   let techElement = document.querySelector('.tech');
 
@@ -34,18 +50,22 @@ function changeTechText(event) {
 
 document.getElementById('input').addEventListener('keyup', changeTechText);
 
-/*  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio? */
-document.getElementById('mySpotrybefy').addEventListener('dblclick', () => window.open('https://renatosbispo.github.io', '_blank'));
+/*
+ * Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
+ * ele redirecione para alguma página.
+ * 1. Que tal redirecionar para seu portifólio?
+ */
+let top3 = document.getElementById('mySpotrybefy');
+top3.addEventListener('dblclick', () => window.open('https://renatosbispo.github.io', '_blank'));
 
 /*
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
+ * Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+ * a cor do mesmo;
+ */
+top3.addEventListener('mouseover', event => event.target.style.color = `rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`);
+top3.addEventListener('mouseleave', event => event.target.style.color = null);
 
-Segue abaixo um exemplo do uso de event.target:
-*/
-
+// Segue abaixo um exemplo do uso de event.target:
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
   event.target.innerText = 'Opção reiniciada';
