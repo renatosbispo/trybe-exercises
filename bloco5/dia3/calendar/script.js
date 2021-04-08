89f57214e6a151869e86f13e7831c0d02ca89130
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-08 11:27:00 -0300

    Finish exercise 2

--- bloco5/dia3/calendar/script.js
+++ bloco5/dia3/calendar/script.js
@@ -14,22 +14,36 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Exercise 1
-const dezDaysList = [28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-let daysList = document.getElementById('days');
-let daysListItem;
+function createDaysOfTheMonth() {
+  const dezDaysList = [28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  let daysList = document.getElementById('days');
+  let daysListItem;
 
-for (let day of dezDaysList) {
-  daysListItem = document.createElement('li');
-  daysListItem.innerText = day;
-  daysListItem.classList.add('day');
+  for (let day of dezDaysList) {
+    daysListItem = document.createElement('li');
+    daysListItem.innerText = day;
+    daysListItem.classList.add('day');
 
-  if (day === 24 || day === 25 || day === 31) {
-    daysListItem.classList.add('holiday');
+    if (day === 24 || day === 25 || day === 31) {
+      daysListItem.classList.add('holiday');
+    }
+
+    if ((day - 3) % 7 === 0) {
+      daysListItem.classList.add('friday');
+    }
+
+    daysList.appendChild(daysListItem);
   }
-
-  if ((day - 3) % 7 === 0) {
-    daysListItem.classList.add('friday');
-  }
-
-  daysList.appendChild(daysListItem);
 }
+
+createDaysOfTheMonth();
+
+// Exercise 2
+function createHolidayButton(holiday) {
+  let holidayButton = document.createElement('button');
+
+  holidayButton.innerText = holiday;
+  holidayButton.id = 'btn-holiday';
+  document.querySelector('.buttons-container').appendChild(holidayButton);
+}
+
