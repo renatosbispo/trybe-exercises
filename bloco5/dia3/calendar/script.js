83d5ba0c062e52cf37f8ecf8fbf20b43aaf84a5e
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-08 11:15:27 -0300

    Finish exercise 1

--- bloco5/dia3/calendar/script.js
+++ bloco5/dia3/calendar/script.js
@@ -1,5 +1,5 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
   const weekDaysList = document.querySelector('.week-days');
 
   for (let index = 0; index < weekDays.length; index += 1) {
@@ -14,6 +14,22 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Exercise 1
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+const dezDaysList = [28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 let daysList = document.getElementById('days');
+let daysListItem;
 
+for (let day of dezDaysList) {
+  daysListItem = document.createElement('li');
+  daysListItem.innerText = day;
+  daysListItem.classList.add('day');
+
+  if (day === 24 || day === 25 || day === 31) {
+    daysListItem.classList.add('holiday');
+  }
+
+  if ((day - 3) % 7 === 0) {
+    daysListItem.classList.add('friday');
+  }
+
+  daysList.appendChild(daysListItem);
+}
