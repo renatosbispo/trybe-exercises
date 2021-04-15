feadfc2d7abc06d65a6c82104fc97a03dc19e39d
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-15 19:25:48 -0300

    Finish exercise 1

---
+++ bloco6/dia1/resume-form/script.js
@@ -0,0 +1,172 @@
+function addStateSelectOptions() {
+  const states = [
+    {
+        "name": "Acre",
+        "initials": "ac"
+    },
+    {
+        "name": "Alagoas",
+        "initials": "al"
+    },
+    {
+        "name": "Amapá",
+        "initials": "ap"
+    },
+    {
+        "name": "Amazonas",
+        "initials": "am"
+    },
+    {
+        "name": "Bahia",
+        "initials": "ba"
+    },
+    {
+        "name": "Ceará",
+        "initials": "ce"
+    },
+    {
+        "name": "Espírito Santo",
+        "initials": "es"
+    },
+    {
+        "name": "Goiás",
+        "initials": "go"
+    },
+    {
+        "name": "Maranhão",
+        "initials": "ma"
+    },
+    {
+        "name": "Mato Grosso",
+        "initials": "mt"
+    },
+    {
+        "name": "Mato Grosso do Sul",
+        "initials": "ms"
+    },
+    {
+        "name": "Minas Gerais",
+        "initials": "mg"
+    },
+    {
+        "name": "Pará",
+        "initials": "pa"
+    },
+    {
+        "name": "Paraíba",
+        "initials": "pb"
+    },
+    {
+        "name": "Paraná",
+        "initials": "pr"
+    },
+    {
+        "name": "Pernambuco",
+        "initials": "pe"
+    },
+    {
+        "name": "Piauí",
+        "initials": "pi"
+    },
+    {
+        "name": "Rio de Janeiro",
+        "initials": "rj"
+    },
+    {
+        "name": "Rio Grande do Norte",
+        "initials": "rn"
+    },
+    {
+        "name": "Rio Grande do Sul",
+        "initials": "rs"
+    },
+    {
+        "name": "Rondônia",
+        "initials": "ro"
+    },
+    {
+        "name": "Roraima",
+        "initials": "rr"
+    },
+    {
+        "name": "Santa Catarina",
+        "initials": "sc"
+    },
+    {
+        "name": "São Paulo",
+        "initials": "sp"
+    },
+    {
+        "name": "Sergipe",
+        "initials": "se"
+    },
+    {
+        "name": "Tocantins",
+        "initials": "to"
+    },
+    {
+        "name": "Distrito Federal",
+        "initials": "df"
+    }
+  ]
+
+  for (let index = 0; index < states.length; index += 1) {
+    const stateSelect = document.getElementById('state-selection');
+    const stateInfo = states[index];
+    const option = document.createElement('option');
+
+    option.value = stateInfo.initials;
+    option.innerText = stateInfo.name;
+    stateSelect.appendChild(option);
+  }
+}
+
+function isNumber(element) {
+  if (element - (element - 1) === 1) {
+    return true;
+  }
+
+  return false;
+}
+
+function getNumbersFromString(initialString) {
+  let numbersOnlyString = '';
+  let currentCharacter = '';
+
+  for (let index = 0; index < initialString.length; index += 1) {
+    currentCharacter = initialString[index];
+    if(isNumber(currentCharacter)) {
+      numbersOnlyString += currentCharacter;
+    }
+  }
+
+  return numbersOnlyString;
+}
+
+function formatCpf(event) {
+  let currentValue = event.target.value;
+  const currentValueNumbersOnly = getNumbersFromString(currentValue);
+
+  if (event.inputType !== 'insertText') {
+    return;
+  }
+
+  if (!isNumber(event.data)) {
+    currentValue = currentValue.substring(0, currentValue.lastIndexOf(event.data));
+    event.target.value = currentValue;
+    return;
+  }
+
+  if (currentValueNumbersOnly.length % 3 === 0 && currentValueNumbersOnly.length < 9) {
+    event.target.value += '.';
+  } else if (currentValueNumbersOnly.length === 9) {
+    event.target.value += '-';
+  }
+}
+
+window.onload = () => {
+  const cpfField = document.getElementById('cpf');
+
+  cpfField.addEventListener('input', formatCpf);
+  addStateSelectOptions();
+};
