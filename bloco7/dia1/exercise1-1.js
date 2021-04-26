6d136bc3fbba9c21221ef014dae804edc9ea7ad3
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-26 17:41:35 -0300

    Add template literals

--- bloco7/dia1/exercise1-1.js
+++ bloco7/dia1/exercise1-1.js
@@ -1,12 +1,11 @@
 const testingScope = escopo => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
+    console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
   } else {
     const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
     console.log(elseScope);
   }
 }
 
-testingScope(false);
+testingScope(true);
