30a324aa7bc5476dbbf113b4cad798c8b1197723
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-26 17:49:20 -0300

    Finish exercise 2 from part 1

--- bloco7/dia1/exercise1-2.js
+++ bloco7/dia1/exercise1-2.js
@@ -2,4 +2,4 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
-console.log(oddsAndEvens);
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b).join(', ')} se encontram ordenados de forma crescente!`);
