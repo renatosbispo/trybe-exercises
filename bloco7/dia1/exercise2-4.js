87ba363acd7baacf51409c8b7c195347a6cd9e86
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-27 01:24:26 -0300

    Finish function 2 from exercise 4 in part 2

--- bloco7/dia1/exercise2-4.js
+++ bloco7/dia1/exercise2-4.js
@@ -1,3 +1,13 @@
+const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'MongoDB'];
 const replaceX = (targetString, replacement) => targetString.replace(/[xX]/g, replacement);
+const attachSkills = (greetings) => {
+  skills.sort();
 
-console.log(replaceX('Tryber x aqui!', 'xablau'));
+  return `${greetings} My top 5 skills are:
+
+- ${skills.join('\n- ')}
+
+#goTrybe!`;
+};
+
+console.log(attachSkills(replaceX('Tryber x here!', 'Renato')));
