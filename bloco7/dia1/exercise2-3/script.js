718c13a1271f98a10741d2136395349164ec1545
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-26 22:42:05 -0300

    Finish exercise 3 from part 2

--- bloco7/dia1/exercise2-3/script.js
+++ bloco7/dia1/exercise2-3/script.js
@@ -1,1 +1,11 @@
-console.log('Script connected!');
+window.onload = () => {
+  let clickCount = 0;
+  const counterDisplay = document.querySelector('.counter-display');
+  const counterButton = document.querySelector('.counter-button');
+
+  counterDisplay.innerHTML = clickCount.toString(10);
+  counterButton.addEventListener('click', () => {
+    clickCount === 99 ? clickCount = 0 : clickCount += 1;
+    counterDisplay.innerHTML = clickCount.toString(10);
+  });
+};
