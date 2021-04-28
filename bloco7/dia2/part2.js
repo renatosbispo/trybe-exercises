b7d5d029035f03a0f6f6bfb3d8cddcfd3ac57e12
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-27 21:14:28 -0300

    Finish exercise 3 from part 2

--- bloco7/dia2/part2.js
+++ bloco7/dia2/part2.js
@@ -18,14 +18,21 @@ const lesson3 = {
   turno: 'noite',
 };
 
+// Exercise 1
 const addLessonInfo = (lesson, infoKey, infoValue) => {
   lesson[infoKey] = infoValue;
 };
+addLessonInfo(lesson2, 'turno', 'manhã');
+console.log(lesson2);
 
+// Exercise 2
 const printKeys = (object) => {
   Object.keys(object).forEach(key => console.log(key));
 };
-
-addLessonInfo(lesson2, 'turno', 'manhã');
-console.log(lesson2);
 printKeys(lesson3);
+
+// Exercise 3
+const getObjectSize = (object) => {
+  return Object.keys(object).length;
+};
+console.log(getObjectSize(lesson1));
