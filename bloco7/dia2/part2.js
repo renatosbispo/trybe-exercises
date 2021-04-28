9f07ef555ce76b91d3061d2ab99faefa1b0f9144
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-27 22:19:43 -0300

    Finish exercise 7 from part 2

--- bloco7/dia2/part2.js
+++ bloco7/dia2/part2.js
@@ -36,5 +36,17 @@ const getObjectSize = (object) => {
 const getObjectValues = object => Object.values(object);
 
 // Exercise 5
-const allLessons2 = Object.assign({}, { lesson1, lesson2, lesson3 });
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 
+// Exercise 6
+const getTotalStudents = (allLessons) => {
+  return Object.values(allLessons).reduce((total, lesson) => total + lesson.numeroEstudantes, 0);
+};
+
+// Exercise 7
+const getValueByIndex = (object, index) => {
+  const objectKeys = Object.keys(object);
+  const requestedKey = Object.keys(object)[index];
+
+  return requestedKey ? object[requestedKey] : null;
+}
