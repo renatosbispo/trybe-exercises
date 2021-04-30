0d72dd3dccc36e6f8144c1adfe41dcd5d28f0676
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-30 18:40:30 -0300

    Finish bonus exercise/item 1

---
+++ bloco8/dia1/bonus-exercise.js
@@ -0,0 +1,31 @@
+// Taken from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const getDragonDamage = () => getRandomIntInclusive(15, dragon.strength);
+
