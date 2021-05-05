590c492d70b51f98e3869a793627ef8b5253c99c
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-05 18:41:39 -0300

    Finish exercise 6

--- bloco8/dia4/exercise6.js
+++ bloco8/dia4/exercise6.js
@@ -4,7 +4,10 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  return students.map((student, i) => ({
+    name: student,
+    average: grades[i].reduce((total, grade) => total + grade) / grades[i].length,
+  }));
 }
 
 const expected = [
