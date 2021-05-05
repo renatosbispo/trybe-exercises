f94b5288623abf5275c298c4611bd1972f42b92a
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-05 17:41:49 -0300

    Finish exercise 1

--- bloco8/dia4/exercise1.js
+++ bloco8/dia4/exercise1.js
@@ -7,7 +7,7 @@ const arrays = [
 ];
 
 function flatten() {
-  // escreva seu código aqui
+  return arrays.reduce((flattened, array) => flattened.concat(array));
 }
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
