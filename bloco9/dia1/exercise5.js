89c1d9298591e15cb2fff1e703b6f391231290f2
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 05:37:28 -0300

    Finish exercise 5

--- bloco9/dia1/exercise5.js
+++ bloco9/dia1/exercise5.js
@@ -10,7 +10,13 @@ const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback, delay) => {
+  const temperature = getMarsTemperature();
+  console.log(`[Received data after ${delay / 1000}s]`)
+  callback(temperature);
+};
 
-
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+const delay1 = messageDelay();
+const delay2 = messageDelay();
+setTimeout(sendMarsTemperature, delay1, temperatureInFahrenheit, delay1); // imprime "It is currently 47ºF at Mars", por exemplo
+setTimeout(sendMarsTemperature, delay2, greet, delay2); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
