3b649ee298b0dc4be187b17af097482407bd0276
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 08:39:06 -0300

    Finish bonus exercise

--- bloco9/dia2/exercise2.js
+++ bloco9/dia2/exercise2.js
@@ -1,13 +1,13 @@
-const myPromise = new Promise((resolve, reject) => {
-  let numbers = Array.from(Array(10).keys()).map(() => Math.random() * (50 - 1) + 1);
-  numbers = numbers.map(x => parseInt(x) ** 2);
+async function promiseFunction() {
+  let numbers = Array.from(Array(10).keys()).map(() => Math.floor(Math.random() * 50 + 1));
+  numbers = numbers.map(x => x ** 2);
   let sum = numbers.reduce((sum, x) => sum + x);
 
-  if (sum < 8000) resolve(sum);
-  else reject(new Error(`[PROMISE REJECTED] Sum = ${sum}`));
-});
+  if (sum < 8000) return sum;
+  else throw new Error(`[PROMISE REJECTED] Sum = ${sum}`);
+}
 
-myPromise
+promiseFunction()
   .then(response => [2, 3, 5, 10].map((x) => response / x))
   .then(result => result.reduce((sum, x) => sum + x))
   .then(finalResult => console.log(finalResult))
