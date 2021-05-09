5995e9bd5360d155c9d7464269c2cc53d58a116b
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 06:36:50 -0300

    Finish experiments with Promises

--- bloco9/dia2/experiments.js
+++ bloco9/dia2/experiments.js
@@ -1,8 +1,11 @@
 const promise = new Promise((resolve, reject) => {
-  const number = Math.floor(Math.random() * 11);
+  const number = Math.floor(Math.random()* 11);
 
   if (number <= 5) {
-    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+    return reject(number);
   }
-  resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
-});
+  resolve(number);
+})
+.then(number => `Que sucesso =) nosso número foi ${number}`)
+.then(msg => console.log(msg))
+.catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
