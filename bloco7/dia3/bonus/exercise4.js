8ee96445dc1bd6709eb6a65627a7f4bdc520e2f5
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 20:13:13 -0300

    Finish bonus exercise 4

--- bloco7/dia3/bonus/exercise4.js
+++ bloco7/dia3/bonus/exercise4.js
@@ -1,12 +1,16 @@
+const assert = require('assert');
+
 const getLargestNumber = (array) => {
-  let largestNumber;
-  for (let index = 0; index < array.length - 1; index += 1) {
-      if (array[index] > array[index + 1]) {
-          largestNumber = [array[index]];
-      }
+  let largestNumber = array[0];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > largestNumber) {
+        largestNumber = array[index];
+    }
   }
   return largestNumber;
 }
 
 const parameter = [45, 8, 2, 50, 1, 7, 99];
 const result = 99;
+
+assert.strictEqual(getLargestNumber(parameter), result);
