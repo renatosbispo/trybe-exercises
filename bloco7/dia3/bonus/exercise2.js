7acae059e4ea0d0952166aadd317e395a6e81e7f
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 19:27:55 -0300

    Finish bonus exercise 2

--- bloco7/dia3/bonus/exercise2.js
+++ bloco7/dia3/bonus/exercise2.js
@@ -1,5 +1,9 @@
 const assert = require('assert');
-// escreva a função factorial aqui
+
+function factorial(n) {
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
+}
 
 const in1 = 5;
 const exp1 = 120;
