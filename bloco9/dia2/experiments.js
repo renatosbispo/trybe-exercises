8dc6fac731c7f6755509035a9a88c4d6793a61e1
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 07:18:10 -0300

    Add async and await experiments

--- bloco9/dia2/experiments.js
+++ bloco9/dia2/experiments.js
@@ -1,11 +1,19 @@
-const promise = new Promise((resolve, reject) => {
-  const number = Math.floor(Math.random()* 11);
+const fetch = require('node-fetch');
 
-  if (number <= 5) {
-    return reject(number);
+async function verifiedFetch(url) {
+  if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => r.value);
   }
-  resolve(number);
-})
-.then(number => `Que sucesso =) nosso número foi ${number}`)
-.then(msg => console.log(msg))
-.catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
+  throw new Error('endpoint não existe');
+}
+
+async function sendJokeToFriend(name) {
+  const message = await verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+    .catch((err) => err);
+  console.log(message);
+}
+
+sendJokeToFriend("Anna");
