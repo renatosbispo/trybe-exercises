47edf2d8660590550ccf42cafbe580a3e34e0e4a
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 08:02:05 -0300

    Finish exercise 1

--- bloco9/dia2/apiScript.js
+++ bloco9/dia2/apiScript.js
@@ -1,8 +1,14 @@
-// apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const requestParameters = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' },
+  }
+
+  fetch(API_URL, requestParameters)
+    .then(response => response.json())
+    .then(data => document.getElementById('jokeContainer').innerText = data.joke);
 };
 
 window.onload = () => fetchJoke();
