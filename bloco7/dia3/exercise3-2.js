6c70931214464f336e397a1bd41e25fe21ac3eb0
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-05-09 18:44:45 -0300

    Finish exercise 3.2

--- bloco7/dia3/exercise3-2.js
+++ bloco7/dia3/exercise3-2.js
@@ -1,18 +1,22 @@
+const assert = require('assert');
+
 const removeVowels = (word) => {
   const characters = word.split('');
-  const results = [];
+  let results = '';
+  let vowels = 0;
 
   for (let index = 0; index < characters.length; index += 1) {
     if (
-      characters[index] === 'a' ||
-      characters[index] === 'o' ||
-      characters[index] === 'i' ||
-      characters[index] === 'e' ||
-      characters[index] === 'u'
+      characters[index].toLowerCase() === 'a' ||
+      characters[index].toLowerCase() === 'o' ||
+      characters[index].toLowerCase() === 'i' ||
+      characters[index].toLowerCase() === 'e' ||
+      characters[index].toLowerCase() === 'u'
     ) {
-      results.push(characters[index]);
+      vowels += 1;
+      results += vowels;
     } else {
-      results.push('_');
+      results += characters[index];
     }
   }
   return results;
@@ -21,3 +25,6 @@ const removeVowels = (word) => {
 
 const parameter = 'Dayane';
 const result = 'D1y2n3';
+
+assert.strictEqual(removeVowels(parameter), result);
+assert.strictEqual(removeVowels('Amanda'), '1m2nd3');
